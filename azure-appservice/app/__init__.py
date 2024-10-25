27eb4c761f7a12d77785cace99263fbaccb6b592
tomrss <tommaso.rossi@pagopa.it>
2024-10-25 21:17:14 +0200

    fix: Homepage

--- azure-appservice/app/__init__.py
+++ azure-appservice/app/__init__.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     env = os.getenv("APP_ENV", "UNKNOWN")
-    return f""""
+    return f"""
     <h2>Welcome to GitHub Workflow Playground</h2>
-    <h3>Azure App Service</h3>A
+    <h3>Azure App Service</h3>
     <p>Environment: <b>{env}</b>
     """
 
