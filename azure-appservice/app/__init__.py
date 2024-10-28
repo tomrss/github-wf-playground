d2b8dea2680caf673288f4be82474cb58220d3ae
tomrss <tommaso.rossi@pagopa.it>
2024-10-28 12:27:34 +0100

    feat: Logging

--- azure-appservice/app/__init__.py
+++ azure-appservice/app/__init__.py
@@ -1,12 +1,18 @@
+import logging
 import os
 from flask import Flask
 
 app = Flask(__name__)
 
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
+app.logger.setLevel(gunicorn_logger.level)
+
 
 @app.route("/")
 def index():
     env = os.getenv("APP_ENV", "UNKNOWN")
+    app.logger.info(f"Requested index for env {env}")
     return f"""
     <h2>Welcome to GitHub Workflow Playground</h2>
     <h3>Azure App Service</h3>
@@ -18,4 +24,5 @@ def index():
 
 @app.route("/health")
 def health():
+    app.logger.debug("health OK")
     return {"status": "OK"}
